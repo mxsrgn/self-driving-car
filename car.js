4cc4997f5195755be97e6c8dc4db8ef4ac62a0f4
Paolo Villagomez <mxsrgn@gmail.com>
2023-05-19 14:04:15 -0700

    debugging sensors

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car{
     this.friction = 0.05
     this.angle = 0
 
+    this.sensor = new Sensor(this)
     this.controls = new Controls()
   }
 
-  update(){
+  update(roadBorders){
     this.#move()
+    this.sensor.update(roadBorders)
   }
 
   #move(){
@@ -33,7 +35,7 @@ class Car{
       this.speed =- this.maxSpeed/2
     }
 
-    if(this.speed < 0){
+    if(this.speed > 0){
       this.speed -= this.friction
     }
     if(this.speed < 0){
@@ -72,6 +74,8 @@ class Car{
     ctx.fill()
 
     ctx.restore()
+
+    this.sensor.draw(ctx)
   }
 }
 
